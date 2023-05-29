f5733fe4f848a73994786056c7f37cc7e8f093f8
mehmet-27 <mehmetofficial68@gmail.com>
2023-05-29 22:10:02 +0300

    Add FabledSkyBlock Support

---
+++ src/main/java/xyz/geik/farmer/integrations/fabledskyblock/FabledSkyListener.java
@@ -0,0 +1,62 @@
+package xyz.geik.farmer.integrations.fabledskyblock;
+
+import com.songoda.skyblock.api.event.island.IslandCreateEvent;
+import com.songoda.skyblock.api.event.island.IslandDeleteEvent;
+import com.songoda.skyblock.api.event.island.IslandLevelChangeEvent;
+import com.songoda.skyblock.api.event.island.IslandOwnershipTransferEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+import xyz.geik.farmer.Main;
+import xyz.geik.farmer.api.FarmerAPI;
+import xyz.geik.farmer.helpers.Settings;
+import xyz.geik.farmer.model.Farmer;
+
+/**
+ * @author mehmet-27
+ */
+public class FabledSkyListener implements Listener {
+
+    /**
+     * Automatically creates a farmer when island is created
+     */
+    @EventHandler
+    public void onIslandCreate(IslandCreateEvent event) {
+        if (Settings.autoCreateFarmer) {
+            new Farmer(event.getIsland().getIslandUUID().toString(), event.getIsland().getOwnerUUID(), 0);
+            Player player = Bukkit.getPlayer(event.getIsland().getOwnerUUID());
+            if (player != null) player.sendMessage(Main.getLangFile().getText("boughtFarmer"));
+        }
+    }
+
+    /**
+     * Remove farmer on island delete
+     */
+    @EventHandler
+    public void onIslandDelete(IslandDeleteEvent event) {
+        FarmerAPI.getFarmerManager().removeFarmer(event.getIsland().getIslandUUID().toString());
+    }
+
+    /**
+     * Change farmer owner on island transfer
+     */
+    @EventHandler
+    public void transferIslandEvent(IslandOwnershipTransferEvent event) {
+        FarmerAPI.getFarmerManager().changeOwner(event.getIsland().getOriginalOwnerUUID(),
+                event.getOwner().getUniqueId(),
+                event.getIsland().getIslandUUID().toString());
+
+    }
+
+    /**
+     * Remove farmer on island reset
+     */
+    @EventHandler
+    public void onIslandResetEvent(@NotNull IslandLevelChangeEvent event) {
+        if (event.getLevel().getLevel() == 0) {
+            FarmerAPI.getFarmerManager().removeFarmer(event.getIsland().getIslandUUID().toString());
+        }
+    }
+}
