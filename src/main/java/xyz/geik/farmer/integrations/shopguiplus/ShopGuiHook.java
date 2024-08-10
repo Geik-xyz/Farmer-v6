1b035696cd6c5efaadc148f1dee06b17d22ea310
mehmet-27 <mehmetofficial68@gmail.com>
2024-08-11 00:45:07 +0300

    Add ShopGuiPlus support + + Add ShopGui+ support for getting item prices from ShopGui+'s custom shops prices.

---
+++ src/main/java/xyz/geik/farmer/integrations/shopguiplus/ShopGuiHook.java
@@ -0,0 +1,23 @@
+package xyz.geik.farmer.integrations.shopguiplus;
+
+import net.brcdev.shopgui.ShopGuiPlusApi;
+import net.brcdev.shopgui.event.ShopsPostLoadEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import xyz.geik.farmer.api.managers.FarmerManager;
+import xyz.geik.farmer.model.inventory.FarmerItem;
+
+public class ShopGuiHook implements Listener {
+
+	@EventHandler
+	public void onShopsPostLoadEvent(ShopsPostLoadEvent event) {
+		FarmerManager.getFarmers().forEach((id, farmer) -> {
+			for (FarmerItem farmerItem : farmer.getInv().getItems()) {
+				double shopGuiPrice = ShopGuiPlusApi.getItemStackPriceSell(farmerItem.getMaterial().parseItem());
+				if (shopGuiPrice != -1) {
+					farmerItem.setPrice(shopGuiPrice);
+				}
+			}
+		});
+	}
+}
