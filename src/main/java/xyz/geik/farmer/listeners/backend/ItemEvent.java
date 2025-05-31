35a68dff3dda50705a70f58137837390a69a6f39
Nitra <nitradosamet@hotmail.com>
2025-05-31 14:08:51 +0300

    Added item-specific permission checks for farmer owner

--- src/main/java/xyz/geik/farmer/listeners/backend/ItemEvent.java
+++ src/main/java/xyz/geik/farmer/listeners/backend/ItemEvent.java
@@ -1,6 +1,7 @@
 package xyz.geik.farmer.listeners.backend;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
